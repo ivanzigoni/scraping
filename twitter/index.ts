8a3ed8183cc1a8d07e6713553d82eb677e3ddbe4
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2022-11-07 11:59:22 -0300

    feat: twitter login lol

--- twitter/index.ts
+++ twitter/index.ts
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer';
+import { credentials as c } from '../credentials';
 
 async function main() {
   const browser = await puppeteer.launch();
@@ -8,7 +9,35 @@ async function main() {
 
   await page.waitForSelector('[autocomplete="username"]');
 
-  await page.type('[autocomplete="username"]', "flamingofeliz")
+  await page.type('[autocomplete="username"]', c.username);
+
+  await page.evaluate(() => {
+    [...document.getElementsByTagName("div")]
+      .filter(div => div.getAttribute("role") === "button")
+      .find(div => [...div.children].find(element => element.textContent === "Next"))
+      ?.setAttribute("nextButton", "true");
+
+    return true
+  })
+
+  await page.click('[nextButton="true"]');
+
+  await page.waitForSelector('[autocomplete="current-password"]');
+
+  await page.type('[autocomplete="current-password"]', c.password);
+
+  await page.evaluate(() => {
+    [...document.getElementsByTagName("div")]
+      .filter(div => div.getAttribute("role") === "button")
+      .find(div => [...div.children].find(element => element.textContent === "Log in"))
+      ?.setAttribute("loginButton", "true");
+
+    return true
+  })
+  
+  await page.click('[loginButton="true"]');
+
+  await page.waitForSelector('[aria-label="Home timeline"]')
 
   await page.screenshot({ path: `./screenshots/${new Date().toISOString()}.jpg`});
 
